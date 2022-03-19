b98d201d801b48b65844e1264222c44f34ec2cab
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2022-03-19 16:14:48 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import database from './database';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalInterceptors(new AuthInterceptor());
   await database.connect();
   await app.listen(process.env.PORT || 3000);
